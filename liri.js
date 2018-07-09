614e71d53a170c134798fc1c0465a5ffce5e6e26
Raquel Azcue <rvazcue@gmail.com>
2018-07-09 12:29:27 -0500

    renamed the env file

--- liri.js
+++ liri.js
@@ -1,5 +1,21 @@
 
+//this is the only line needed to 'require'  the dotenv module
 require("dotenv").config();
+
+//requiring the following npm modules: Twitter, Spotify API OMDB API
+
+var Twitter = require('twitter');
+
+//Per the documentation the snippet below uses enviormental variables to store 
+//the twitter keys 
+//this approach is preferred when developers want to keep some information private 
+var client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  });
+
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
