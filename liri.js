72b36082544039d056b2b6200604f4e834adeb47
Raquel Azcue <rvazcue@gmail.com>
2018-07-09 17:16:16 -0500

    sending updated logic for the twitter command

--- liri.js
+++ liri.js
@@ -30,14 +30,11 @@ var inquirer = require("inquirer");
     // If the inquirerResponse confirms, we then use a switch
     //statement to run the specified code
     if (inquirerResponse.confirm) {
-      console.log("Your " + inquirerResponse.SelectedCommand + " is ready for battle!");
+      console.log("You selected " + inquirerResponse.SelectedCommand );
       switch (inquirerResponse.SelectedCommand) {
 
         case 'my-tweets':
-            console.log('this section contains the code for the twitter selection');
-            //Per the Twitter module docs the snippet 
-            //below uses enviormental variables to store  the twitter keys 
-            //this approach is preferred when developers want to keep some information private 
+        
             inquirer.prompt([ 
                 { type: "input",
                 message: "What is your twitter handle (without the '@')?",
@@ -52,42 +49,29 @@ var inquirer = require("inquirer");
 
                 console.log("here is the username entered: " + JSON.stringify(result.username))
                  
+                //Per the Twitter module docs the snippet 
+                //below uses enviormental variables to store  the twitter keys 
+                //this approach is preferred when developers want to keep some information private 
                 var client = new Twitter({
                 consumer_key: process.env.TWITTER_CONSUMER_KEY,
                 consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
                 access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
                 access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
                 })
-               
-                
-                    // https://dev.twitter.com/rest/reference/get/statuses/user_timeline
-                    client.get('statuses/user_timeline', { screen_name: result.username, count: 20 }, function(error, tweets) {
+            
+                client.get('statuses/user_timeline', { screen_name: result.username, count: 20 }, function(error, tweets) {
                       if (error != null) {
                         console.log("Error " + JSON.stringify(error, null, 2))
-                      }
+                        }
                       else {
-                        
-                        let singleTweet = "\nHere is the index 0 of the tweet object: \n" + JSON.stringify(tweets[0].text, null, 2)
+                          for (let x = 0 ; x < tweets.length ; x++){
+                        let singleTweet = `\nHere is the index ${x} of the tweet object: \n` + JSON.stringify(tweets[x].text, null, 2)
                         console.log(singleTweet)
-                        console.log("the length of the tweets: " + tweets.length)
-                      }
+                        console.log("the length of the tweets: " + tweets.length)}
+                        }
                     });
-                 
-                // client.get('search/tweets', {q: 'cats'}, function(error, tweets, response) {
-                // console.log(tweets);});
-            })
-            //now that the 'client'  has been declared into existence 
-            //we can  use the template below to 'get' the desired tweets
-            //template: client.get(path, params, callback);
-            //the parameters do the following
-            //path sets the path to 'search/tweets'
-            //the {q: 'cats'} states that we are querying the term 'cats'
-            //rgw last paramater become an anon functions that recieves the 
-            //error msg, 15 tweets & reponse info
-            //here i can customise the search query term to what evs i like
-            //perhaps change this to a prompt asking what to search for?
-           
-             
+
+                })  
             break;
         
         case 'spotify-this-song':
