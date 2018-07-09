9343275608b704d7e64a1c30efb29eb2c78515ef
Raquel Azcue <rvazcue@gmail.com>
2018-07-09 13:26:21 -0500

    updated the liri.js file

--- liri.js
+++ liri.js
@@ -2,12 +2,17 @@
 //this is the only line needed to 'require'  the dotenv module
 require("dotenv").config();
 
-//requiring the following npm modules: Twitter, Spotify API OMDB API
-
+//requiring the following npm modules: Twitter, Spotify API & Inquirer
+//Note: To request info to OMDB API the Request will be used
 var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+var inquirer = require("inquirer");
 
-//Per the documentation the snippet below uses enviormental variables to store 
-//the twitter keys 
+
+
+
+//Per the Twitter module docs the snippet 
+//below uses enviormental variables to store  the twitter keys 
 //this approach is preferred when developers want to keep some information private 
 var client = new Twitter({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -17,5 +22,79 @@ var client = new Twitter({
   });
 
 
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+  //here i will create  prompt that asks which of the commands ou would like to run
+  inquirer
+  .prompt([
+    // Here we create a basic text prompt.
+    {
+      type: "input",
+      message: "What is your name?",
+      name: "username"
+    },
+    {
+      type: "confirm",
+      message: "Are you sure:",
+      name: "confirm",
+      default: true
+    },
+    // Here we create a basic password-protected text prompt.
+    {
+      type: "password",
+      message: "Set your password",
+      name: "password"
+    },
+    {
+      type: "confirm",
+      message: "Are you sure:",
+      name: "confirm",
+      default: true
+    },
+    // Here we give the user a list to choose from.
+    {
+      type: "list",
+      message: "Which Pokemon do you choose?",
+      choices: ["Bulbasaur", "Squirtle", "Charmander"],
+      name: "pokemon"
+    },
+    // Here we ask the user to confirm.
+    {
+      type: "confirm",
+      message: "Are you sure:",
+      name: "confirm",
+      default: true
+    }
+  ])
+  .then(function(inquirerResponse) {
+    // If the inquirerResponse confirms, we displays the inquirerResponse's username and pokemon from the answers.
+    if (inquirerResponse.confirm) {
+      console.log("\nWelcome " + inquirerResponse.username);
+      console.log("Your " + inquirerResponse.pokemon + " is ready for battle!\n");
+    }
+    else {
+      console.log("\nThat's okay " + inquirerResponse.username + ", come again when you are more sure.\n");
+    }
+  });
+
+
+
+
+//var spotify = new Spotify(keys.spotify);
+//var client = new Twitter(keys.twitter);
+
+
+ 
+// var spotify = new Spotify({
+//   id: 
+//   secret: 
+// });
+ 
+// spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+//   if (err) {
+//     return console.log('Error occurred: ' + err);
+//   }
+ 
+// console.log(data); 
+// });
+
+
+
